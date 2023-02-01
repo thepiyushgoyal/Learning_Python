f1e90a5ac9a19bb3ee2556f0fc004e12c3d73fc6
Piyush <piyushgoyal833@gmail.com>
2023-02-01 21:20:39 +0530

    changed28file

--- 28_osModule.py
+++ 28_osModule.py
@@ -9,16 +9,18 @@ os.system('cls')
 # if (not os.path.exists("28_osModule/1 Created")):
 #     for i in range(1, 6):
 #         os.mkdir(f"28_osModule/{i} Created")
-
-# if (not os.path.exists("28_osModule/1 filed")):
-#     for i in range(1, 6):
-#         os.rename(f"28_osModule/{i} Created", f"28_osModule/{i} filed")
+j = 1
+path = os.chdir("D:\\CODING\\28_osModule")
+# if (not os.path.exists("28_osModule/1")):
+for folder in os.listdir(path):
+    os.rename(folder, f"{j}fol")
+    j += 1
 
 # folderinDailyPython = os.listdir("28_osModule")
 # print(folderinDailyPython)
 # #  }
-folders = os.listdir("python")
-print(folders)
+# folders = os.listdir("python")
+# print(folders)
 
-for folder in folders:
-    print(os.listdir(f"python/{folder}"))
+# for folder in folders:
+#     print(os.listdir(f"python/{folder}"))
