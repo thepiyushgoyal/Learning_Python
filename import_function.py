c726dd7d81f2c9c8da3f497549405cc19087ddf3
Piyush <piyushgoyal833@gmail.com>
2023-02-06 18:43:53 +0530

    chanedImp

--- import_function.py
+++ import_function.py
@@ -1,7 +1,23 @@
 import random as rand
-import os
-# os.system('cls')
+def randomNamesGenerator(num,words):
+    alphaList = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+    alphaLowerlist = alphaList.split(",")
+    alphaUpperlist = alphaList.swapcase().split(",")
+    randNames = list()
+    for i in range(0,num):
+        randAlphaS = list()
+        for j in range(0,words):
+            randAlpha = alphaLowerlist[rand.randint(0,25)]
+            randAlphaS.append(randAlpha)
+        randFourAlphaWord = ""
+        rfaw = randFourAlphaWord.join(randAlphaS)
+        randName = alphaUpperlist[rand.randint(0,25)]+rfaw
+        randNames.append(randName)
+    return randNames
 
+# namelist = randomNamesGenerator()
+# print(namelist)
+        
 def fact(n):
     if (n == 0 or n == 1):
         return 1
@@ -23,26 +39,3 @@ def fab(index):
 #     print(fact(4))
 # after doing it comment-out comment the below statement
 # print(fact(4))
-
-def randomNamesGenerator(num,words):
-    alphaList = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
-    alphaLowerlist = alphaList.split(",")
-    alphaUpperlist = alphaList.swapcase().split(",")
-    randNames = list()
-    for i in range(0,num):
-        randAlphaS = list()
-        for j in range(0,words):
-            # randAlphanum = rand.choice(0,25)
-            randAlpha = alphaLowerlist[rand.randint(0,25)]
-            randAlphaS.append(randAlpha)
-            # print(randAlphaS)
-        randFourAlphaWord = ""
-        rfaw = randFourAlphaWord.join(randAlphaS)
-        # print(rfaw)
-        randName = alphaUpperlist[rand.randint(0,25)]+rfaw
-        # print(randName)
-        randNames.append(randName)
-    return randNames
-
-# namelist = randomNamesGenerator()
-# print(namelist)
