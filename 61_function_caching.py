e37d9383037c69634dff9cb736edf875cdd02ef4
Piyush <piyushgoyal833@gmail.com>
2023-07-05 22:21:19 +0530

    ch

---
+++ 61_function_caching.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+import time
+import os
+os.system('cls')
+ 
+
+
+@lru_cache(maxsize=None)
+def fx(n):
+  time.sleep(3)
+  return n*5
+    
+
+print(fx(20))
+print("done for 20")
+print(fx(2))
+print("done for 2")
+print(fx(6))
+print("done for 6")
+
+print(fx(20))
+print("done for 20")
+print(fx(2))
+print("done for 2")
+print(fx(6))
+print("done for 6")
+print(fx(61))
+print("done for 61")
